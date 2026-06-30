b808a338e7431a8e11fb7a183e8e6d38de370736
frzvision-design <frzvision@gmail.com>
2026-06-30 12:44:12 -0300

    Create GitHub configuration in config.js
    Add GitHub configuration settings for repository access.

---
+++ config.js
@@ -0,0 +1,6 @@
+const GITHUB_CONFIG = {
+    owner: 'frzvision-design',
+    repo: 'ksr_task',
+    token: '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',
+    apiUrl: 'https://api.github.com'
+};
