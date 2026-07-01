590fae411e58d88d5219ac7effe2cf9f234c4a64
frzvision-design <farzvision.design@gmail.com>
2026-06-30 23:55:41 -0300

    Remove config.js from tracking

--- config.js
+++
@@ -1,8 +0,0 @@
-// GitHub API Configuration
-// این فایل را در .gitignore قرار دهید تا آپلود نشود
-const GITHUB_CONFIG = {
-    owner: 'frzvision-design',
-    repo: 'ksr_task',
-    token: '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',
-    apiUrl: 'https://api.github.com'
-};
